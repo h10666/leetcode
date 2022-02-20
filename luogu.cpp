9169c0301f061538545b9924699d20aaf3824bec
h10666 <1119527015@qq.com>
2022-02-20 08:52:37 +0800

    2-20

--- luogu.cpp
+++ luogu.cpp
@@ -1,26 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
-int main () {
-	int boxsize,n;
-    cin>>boxsize>>n;
-    vector<int> things(n);
-    vector<int> dp(boxsize+1);dp[boxsize] = 1;
-    for(int i = 0; i<n; i++){
-        int a;cin>>a;
-        things[i] = a;
+int dirs[4][2] = {{1,0},{-1,0},{0,1},{0,-1}};
+int n,m;
+vector<vector<int>> dict;
+vector<vector<int>> visited;
+void dfs(vector<vector<int>>& dict,int x, int y,int &ret, int last){
+    for(int i=0;i<4; i++){
+        int newx = x+dirs[i][0], newy = y+dirs[i][1];
+        if(newx<0||newy<0||newx>=n||newy>=n||dict[newx][newy]==last||visited[newx][newy]==0)continue;
+        visited[newx][newy] = 0;
+        ret++;
+        dfs(dict, newx,newy,ret,dict[newx][newy]);
     }
-    for(int i = 0; i<n; i++){
-		for(int j = boxsize; j>=0; j--){
-			if(dp[j]&&things[i]<=j){
-				dp[j-things[i]] = 1;
-			}
-		}
-	}
-    for(int i = 0; i<=boxsize; i++){
-        if(dp[i]){
-            cout<<i;
-            break;
+}
+int main () {
+	cin>>n>>m;
+    dict = vector<vector<int>>(n,vector<int>(n));
+    
+    for(int i=0;i<n;i++){
+        string s;cin>>s;
+        for (int j = 0; j < n; j++)
+        {
+            dict[i][j] = s[j];
         }
+        
+    }
+    while(m--){
+        int a,b, ret = 0;
+        visited = vector<vector<int>>(n,vector<int>(n,1));
+        cin>>a>>b;
+        a--;b--;
+        dfs(dict,a,b,ret,!dict[a][b]);
+        cout<<ret<<endl;
     }
 	return 0;
 }
